6bc8c2221b6c49bdbddc339020de5d4bf209bbe4
ilka.xd <ilka.xd@yandex.ru>
2023-11-07 20:47:35 +0300

    first review

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function creatCard(card, deleteFnc) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImg = cardElement.querySelector('.card__image');
+
+    cardImg.src = card.link;
+    cardImg.alt = card.name;
+    cardElement.querySelector('.card__title').textContent = card.name;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFnc);
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteFnc(card) {
+    card.target.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => {
+    placesList.append(creatCard(card, deleteFnc));
+});
